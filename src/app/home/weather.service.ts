e230321768779a1e5b4ea6e005da52546f754dcc
Sushil Gangavati <61023801+Sush0220@users.noreply.github.com>
2024-04-04 16:58:00 +0530

    Update weather.service.ts

--- src/app/home/weather.service.ts
+++ src/app/home/weather.service.ts
@@ -10,6 +10,6 @@ export class WeatherService {
   constructor(private http: HttpClient) { }
 
   getWeather(city: string, units: string) {
-    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=50ac3c583588487e42ab83f508e57308&units=${units}`)
+    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid={YOUR_API_KEY}&units=${units}`)
   }
 }
