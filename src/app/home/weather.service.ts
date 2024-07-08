11e56b1488e6d1a0d451e71c6bc737438bed61bd
Sush0220 <sushil.gangavati@gmail.com>
2024-07-08 12:46:41 +0530

    Updates

--- src/app/home/weather.service.ts
+++ src/app/home/weather.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +10,6 @@ export class WeatherService {
   constructor(private http: HttpClient) { }
 
   getWeather(city: string, units: string) {
-    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid={process.env.YOUR_API_KEY}&units=${units}`)
+    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${environment.WEATHER_API_KEY}&units=${units}`)
   }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    WEATHER_API_KEY: '50ac3c583588487e42ab83f508e57308'
+};
